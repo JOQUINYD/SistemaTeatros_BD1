6444dfa37d8a87451629dcaef5c918a50a84bfae
JOQUINYD <54180615+JOQUINYD@users.noreply.github.com>
2020-07-09 16:15:49 -0600

    Primeras Tablas

--- SistemaTeatros_BD1/SistemaTeatrosBD/dbo/Tables/Producciones.sql
+++ SistemaTeatros_BD1/SistemaTeatrosBD/dbo/Tables/Producciones.sql
@@ -1,10 +1,12 @@
 ﻿CREATE TABLE [dbo].[Producciones]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT NOT NULL  IDENTITY (1, 1), 
     [NombreObra] VARCHAR(50) NOT NULL, 
     [FechaInit] DATE NOT NULL, 
     [FechaFin] DATE NOT NULL, 
     [Tipo] VARCHAR(50) NOT NULL, 
     [IdTeatro] INT NOT NULL, 
+    CONSTRAINT [PK_Producciones] PRIMARY KEY ([Id]),
     CONSTRAINT [FK_Producciones_Teatros] FOREIGN KEY ([IdTeatro]) REFERENCES [Teatros]([Id])
+    
 )
--- SistemaTeatros_BD1/SistemaTeatrosBD/dbo/Tables/Teatros.sql
+++ SistemaTeatros_BD1/SistemaTeatrosBD/dbo/Tables/Teatros.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[Teatros]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL IDENTITY(1, 1), 
+    [Nombre] VARCHAR(50) NOT NULL, 
+    [Boleteria] INT NOT NULL, 
+    [Email] NVARCHAR(320) NOT NULL, 
+    [SitioWeb] NVARCHAR(500) NOT NULL, 
+    [Telefono] INT NOT NULL, 
+    CONSTRAINT [PK_Teatros] PRIMARY KEY ([Id])
 )

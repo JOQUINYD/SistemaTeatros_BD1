d9036eebb8e75af2c45d003145899c34d0a1f292
JoalG <53712282+JoalG@users.noreply.github.com>
2020-07-23 19:32:37 -0600

    Registros
    Calendarizacion

--- SistemaTeatros_BD1/SistemaTeatroWebApp/Models/Model1.Designer.cs
+++ SistemaTeatros_BD1/SistemaTeatroWebApp/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Users\Joaquin\Documents\GitHub\SistemaTeatros_BD1\SistemaTeatros_BD1\SistemaTeatroWebApp\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\joalg\Documents\SistemaTeatros_BD1\SistemaTeatros_BD1\SistemaTeatroWebApp\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ SistemaTeatros_BD1/SistemaTeatrosBD/dbo/Tables/Reportes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Reportes]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(0,1), 
+    [Fecha] DATE NOT NULL, 
+    [Cantidad] INT NOT NULL, 
+    [CostoPromedio] DECIMAL(19, 4) NOT NULL, 
+   
+    
+
+)

d36d69bd2f938cf7e37680383e93b72c78d59ba0
JOQUINYD <54180615+JOQUINYD@users.noreply.github.com>
2020-07-26 13:41:48 -0600

    Add ComprarBoletos Agentes
    -Solo por tarjeta de credito

--- SistemaTeatros_BD1/SistemaTeatroWebApp/Controllers/TeatroAgenteController.cs
+++ SistemaTeatros_BD1/SistemaTeatroWebApp/Controllers/TeatroAgenteController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -212,6 +214,182 @@ namespace SistemaTeatroWebApp.Controllers
 
             ViewBag.IdBloque = new SelectList(db.Bloques.Where(b => b.IdTeatro == pad.IdTeatro), "Id", "NombreBloque");
             return View(pad);
+        }
+
+        [AuthorizeUser(IdAcceso: 2)]
+        public ActionResult EscogerBloque(int? IdPresentacion)
+        {
+            var presentacionInfo = db.spGetPresentacionById(IdPresentacion).FirstOrDefault();
+            CompraBoleto cp = new CompraBoleto
+            {
+                IdPresentacion = IdPresentacion,
+                NombreObra = presentacionInfo.NombreObra,
+                NombreTeatro = presentacionInfo.NombreTeatro,
+                IdTeatro = presentacionInfo.IdTeatro,
+                Fecha = presentacionInfo.Fecha,
+                Hora = presentacionInfo.Hora,
+                cantidadAsientos = 1
+            };
+
+            ViewBag.IdBloque = new SelectList(db.Bloques.Where(t => t.IdTeatro == cp.IdTeatro), "Id", "NombreBloque");
+            return View(cp);
+        }
+
+        [HttpPost]
+        [AuthorizeUser(IdAcceso: 2)]
+        public ActionResult EscogerBloque(CompraBoleto cp)
+        {
+            Console.WriteLine(cp.IdPresentacion);
+            Console.WriteLine(cp.IdPresentacion);
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("EscogerFila", new { IdPresentacion = cp.IdPresentacion, IdBloque = cp.IdBloque });
+            }
+
+            var presentacionInfo = db.spGetPresentacionById(cp.IdPresentacion).FirstOrDefault();
+            cp = new CompraBoleto
+            {
+                IdPresentacion = presentacionInfo.Id,
+                NombreObra = presentacionInfo.NombreObra,
+                NombreTeatro = presentacionInfo.NombreTeatro,
+                IdTeatro = presentacionInfo.IdTeatro,
+                Fecha = presentacionInfo.Fecha,
+                Hora = presentacionInfo.Hora
+            };
+            ViewBag.IdBloque = new SelectList(db.Bloques.Where(t => t.IdTeatro == cp.IdTeatro), "Id", "NombreBloque");
+            return View(cp);
+        }
+
+        [AuthorizeUser(IdAcceso: 2)]
+        public ActionResult EscogerFila(int? IdPresentacion, int? IdBloque)
+        {
+            var presentacionInfo = db.spGetPresentacionById(IdPresentacion).FirstOrDefault();
+            var bloqueInfo = db.spGetInfoBloqueById(IdBloque).FirstOrDefault();
+            CompraBoleto cp = new CompraBoleto
+            {
+                IdPresentacion = IdPresentacion,
+                NombreObra = presentacionInfo.NombreObra,
+                NombreTeatro = presentacionInfo.NombreTeatro,
+                IdTeatro = presentacionInfo.IdTeatro,
+                Fecha = presentacionInfo.Fecha,
+                Hora = presentacionInfo.Hora,
+                IdBloque = IdBloque,
+                NombreBloque = bloqueInfo.NombreBloque,
+                asientos = new List<AsientosDisponibles>()
+            };
+
+            var filas = db.spGetFilasByBloque(cp.IdBloque);
+            foreach (var item in filas)
+            {
+
+                string asientosDisp = string.Join(" - ", db.spGetAsientosVaciosFila(cp.IdPresentacion, cp.IdBloque, item.Letra));
+
+                cp.asientos.Add(new AsientosDisponibles
+                {
+                    Letra = item.Letra,
+                    asientos = asientosDisp
+                });
+            }
+
+            ViewBag.Letra = new SelectList(db.Filas.Where(f => f.IdBloque == IdBloque), "Letra", "Letra");
+            return View(cp);
+        }
+
+        [HttpPost]
+        [AuthorizeUser(IdAcceso: 2)]
+        public ActionResult EscogerFila(CompraBoleto cp)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ComprarBoletos", new { IdPresentacion = cp.IdPresentacion, IdBloque = cp.IdBloque, Letra = cp.Letra, cantAsientos = cp.cantidadAsientos });
+            }
+            return View();
+        }
+
+        [AuthorizeUser(IdAcceso: 2)]
+        public ActionResult ComprarBoletos(int? IdPresentacion, int? IdBloque, string Letra, int cantAsientos)
+        {
+            var presentacionInfo = db.spGetPresentacionById(IdPresentacion).FirstOrDefault();
+            var bloqueInfo = db.spGetInfoBloqueById(IdBloque).FirstOrDefault();
+            var precioBloque = db.spGetPrecioByPresBloque(IdPresentacion, IdBloque).FirstOrDefault();
+            CompraBoleto cp = new CompraBoleto
+            {
+                IdPresentacion = IdPresentacion,
+                NombreObra = presentacionInfo.NombreObra,
+                NombreTeatro = presentacionInfo.NombreTeatro,
+                IdTeatro = presentacionInfo.IdTeatro,
+                IdBloque = IdBloque,
+                NombreBloque = bloqueInfo.NombreBloque,
+                Letra = Letra,
+                Fecha = presentacionInfo.Fecha,
+                Hora = presentacionInfo.Hora,
+                asientos = new List<AsientosDisponibles>(),
+                factura = new Factura(),
+                cantidadAsientos = cantAsientos,
+                precioTotal = (decimal)precioBloque * cantAsientos
+            };
+
+
+            string asientosDisp = string.Join(" - ", db.spGetAsientosVaciosFila(cp.IdPresentacion, cp.IdBloque, cp.Letra));
+
+            cp.asientos.Add(new AsientosDisponibles
+            {
+                Letra = cp.Letra,
+                asientos = asientosDisp
+            });
+
+            return View(cp);
+        }
+
+        [HttpPost]
+        [AuthorizeUser(IdAcceso: 2)]
+        public ActionResult ComprarBoletos(CompraBoleto cp, int[] Dist)
+        {
+            // int[] Dist, string Nombre, int id, 
+
+            var builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"DESKTOP-ED7N4A2";
+            builder.InitialCatalog = "SistemaTeatros_BD1";
+            builder.IntegratedSecurity = true;
+
+            DateTime currentDate = DateTime.Now;
+
+            var connectionString = builder.ToString();
+
+            using (SqlConnection sql = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("spCompraBoletosYFactura", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add(new SqlParameter("@Nombre_Cliente", cp.factura.NombreCliente));
+                    cmd.Parameters.Add(new SqlParameter("@Email", cp.factura.Email));
+                    cmd.Parameters.Add(new SqlParameter("@Fecha", currentDate));
+                    cmd.Parameters.Add(new SqlParameter("@Telefono", cp.factura.Telefono));
+                    cmd.Parameters.Add(new SqlParameter("@Hora", currentDate.TimeOfDay));
+                    cmd.Parameters.Add(new SqlParameter("@Numero_Aprobacion", 75));
+                    cmd.Parameters.Add(new SqlParameter("@Boletos", getBoletosTable(cp.IdPresentacion, cp.IdBloque, cp.Letra, Dist)));
+                    sql.Open();
+                    cmd.ExecuteNonQuery();
+                    sql.Close();
+                }
+            }
+            return RedirectToAction("Cartelera");
+        }
+
+        public DataTable getBoletosTable(int? IdPresentacion, int? IdBloque, string Letra, int[] dist)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("IdPresentacion");
+            dt.Columns.Add("LetraFila");
+            dt.Columns.Add("IdBloqueFila");
+            dt.Columns.Add("NumAsiento");
+
+            foreach (var asiento in dist)
+            {
+                dt.Rows.Add(IdPresentacion, Letra, IdBloque, asiento);
+            }
+
+            return dt;
         }
     }
 }
